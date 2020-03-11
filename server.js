1dc9c909334dfb3d880fa3711dd91f560197cc78
Garik Mikaelian <g.b.mikaelian@gmail.com>
2020-03-11 15:49:19 +0400

    Some corections

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ import { api as apiRoutes, auth as authRoutes } from './routes';
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/your-db', { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGO_URL, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(passport.initialize());
 
@@ -25,4 +24,4 @@ app.use('/auth', authRoutes);
 app.use('/api', passport.authenticate('jwt', { session: false }), apiRoutes);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Server is listening at port ${port}`));
